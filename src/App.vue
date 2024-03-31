6f1532c2b437d63120c8ff7b7dbe5722bbb278ac
JS <35227625+just-ajs@users.noreply.github.com>
2024-03-31 16:28:05 -0400

    top bar

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,32 @@ const dropdownOptions = [
 </script>
 
 <template>
-  <div id="content">
-    <div id="sidebar">
-      <h2>Things</h2>
-      <button>ui example</button>
-      <Slider title="Inputtitle" :min="0" :max="50" :step="2"></Slider>
-      <Dropdown title="Dropdown title" :options="dropdownOptions" ></Dropdown>
 
+
+    <div id="top-bar">
+
+      <div id="title-container">
+        <img class="logo-image" alt="Iaac logo" src="./assets/graphics/AECtech_24_Barcelona-Banner_sm.png" />
+
+        <h2>
+          My nice app
+        </h2>
+      </div>
     </div>
 
-    <div id="viewer">
+    <div id="content">
+      <div id="sidebar">
+        <h2>Things</h2>
+        <button>ui example</button>
+        <Slider title="Inputtitle" :min="0" :max="50" :step="2"></Slider>
+        <Dropdown title="Dropdown title" :options="dropdownOptions"></Dropdown>
+
+      </div>
+
+      <div id="viewer">
+      </div>
     </div>
-  </div>
+
 </template>
 
 <script setup>
@@ -34,16 +48,43 @@ const dropdownOptions = [
 #content {
   display: flex;
   flex-direction: row;
-  height: calc(100vh - 68px);
+
+  margin-top: 7cqi;
 }
 
 #sidebar {
+  display: grid;
   width: 310px;
   padding: 10px;
-  flex-shrink: 0; 
+  flex-shrink: 0;
 }
 
 #viewer {
-  flex-grow: 1; 
+  flex-grow: 1;
+}
+
+#top-bar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0 8px 15px -4px rgba(0, 0, 0, 0.2),
+    0 -4px 6px -2px rgba(255, 255, 255, 0.7);
+    position: fixed;
+  top: 0; 
+  left: 0; 
+  width: 100%; 
+  z-index: 1000; 
+  padding-bottom: 10px;
+}
+
+#title-container {
+  display: flex;
+  align-items: center;
+  margin-right: 1.5rem;
+}
+
+.logo-image {
+  height: 4rem;
+  padding: 0.5rem;
 }
 </style>
