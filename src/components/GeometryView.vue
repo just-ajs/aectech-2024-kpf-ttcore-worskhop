888bc2685bb8dbed3613183dec9a9ac9b42f49b2
sophXmoore1 <116595477+sophXmoore1@users.noreply.github.com>
2024-10-18 12:55:13 -0400

    Fix Lights

--- src/components/GeometryView.vue
+++ src/components/GeometryView.vue
@@ -39,8 +39,8 @@ watch(() => props.data, (newValue) => {
 // Three js objects
 let renderer, camera, scene,  controls, container, gui
 let mirrorMaterial, blackMaterial
-let spotLightRed, spotLightBlue, spotLightGreen, spotLightOrange
-
+let spotLight, lightHelper
+let spotLight2, lightHelper2
 
 function init() {
 
@@ -51,7 +51,7 @@ function init() {
   renderer = new THREE.WebGLRenderer()
   renderer.setSize(container.offsetWidth, container.offsetHeight)
   renderer.toneMapping = THREE.ACESFilmicToneMapping;
-  renderer.toneMappingExposure = 0.8
+  renderer.toneMappingExposure = 0.6
 
   // We are taking element defined in <template> and appending our render to it. 
   container.appendChild(renderer.domElement)
@@ -63,7 +63,6 @@ function init() {
 
   // Create scene - https://threejs.org/docs/?q=scene#api/en/scenes/Scene
   scene = new THREE.Scene()
-  scene.rotation.y = 0.5;
 
   // orbit controls - https://threejs.org/docs/#examples/en/controls/OrbitControls
   controls = new OrbitControls(camera, renderer.domElement)
@@ -73,49 +72,52 @@ function init() {
   ambientlight.position.set(0, 0, 0)
 
   //Colored Spot Lights
-  spotLightRed = new THREE.SpotLight( 0xff0040, 400 );
-  spotLightRed.position.set( 100, 0, 0);
-  scene.add( spotLightRed );
-
-  spotLightBlue = new THREE.SpotLight( 0x0040ff, 400 );
-  spotLightBlue.position.set( 0, 100, 0);
-  scene.add( spotLightBlue );
-
-  spotLightGreen = new THREE.SpotLight( 0x80ff80, 400 );
-  spotLightGreen.position.set( -100, 0, 0);
-  scene.add( spotLightGreen );
-
-  spotLightOrange = new THREE.SpotLight( 0xffaa00, 400 );
-  spotLightOrange.position.set( 0, -100, -100);
-  scene.add( spotLightOrange );
+  spotLight = new THREE.SpotLight( 0xff0000, 500 );
+  spotLight.position.set(0, 100, 100 );
+  spotLight.angle = Math.PI / 6;
+  spotLight.penumbra = 1;
+  spotLight.decay = 0;
+  spotLight.distance = 500;
+  scene.add( spotLight );
+  lightHelper = new THREE.SpotLightHelper( spotLight );
+	scene.add( lightHelper );
 
   //Create Materials
   mirrorMaterial = new THREE.MeshStandardMaterial( {
     roughness: 0,
     metalness: 1,
-    flatShading: false
+    envMapIntensity: 1 
   });
-
   blackMaterial = new THREE.MeshBasicMaterial( { color: 0x000000 } ); 
 
   //Load 360 Image for background and envMap
-  new RGBELoader().load(context, function (texture){
+  new RGBELoader().load(context, function (texture) {
     texture.mapping = THREE.EquirectangularReflectionMapping;
-    mirrorMaterial.envMap = texture
+    mirrorMaterial.envMap = texture;
     scene.background = texture;
-  })
+  });
 
-  //Add THREE.js GUI to modify material properties
+  //Add THREE.js GUI to modify properties
   gui = new GUI();
   gui.domElement.style.position = 'absolute';
   gui.domElement.style.top = '95px';
   gui.domElement.style.right = '20px'
-  gui.add( mirrorMaterial, 'roughness', 0, 1 );
-  gui.add( mirrorMaterial, 'metalness', 0, 1 );
-  gui.add( renderer, 'toneMappingExposure', 0, 2 ).name( 'exposure' );
 
-  //Rotate scene
-  controls.autoRotate = true;
+  //mirror material gui
+  gui.add( mirrorMaterial, 'roughness', 0, 1 ).name('discoball roughness')
+  gui.add( mirrorMaterial, 'metalness', 0, 1 ).name('discoball metalness')
+
+  //ambient light gui
+  gui.add( renderer, 'toneMappingExposure', 0, 2 ).name( 'ambient light' );
+
+  //spotlight gui
+  const lightColor = { color: spotLight.color.getHex() };
+  gui.addColor(lightColor, 'color').name('spotlight color').onChange(function (value) {
+    spotLight.color.setHex(value);
+  });
+  gui.add( spotLight,'intensity', 0, 500).name('spotlight intensity')
+  gui.add( spotLight, 'distance', 0, 700).name('spotlight distance')
+  gui.add( spotLight, 'angle', 0, Math.PI / 3).name('spotlight angle')
 
   //Animate
   animate()
@@ -163,7 +165,7 @@ async function compute() {
   });
 }
 
-
+let angle = 0
 
 // for controls update
 function animate() {
@@ -172,24 +174,20 @@ function animate() {
   requestAnimationFrame(animate);
   controls.update();
 
-  //Rotate Lights
-  const time = Date.now() * 0.001;
-  spotLightRed.position.x = Math.sin( time * 0.7 ) * 100;
-  spotLightRed.position.y = Math.cos( time * 0.5 ) * 100;
-  spotLightRed.position.z = Math.cos( time * 0.3 ) * 100;
+  //Rotate Spotlight
+  // const radius = 100; 
+  // angle += 0.01;
+  // spotLight.position.x = Math.sin(angle) * radius;
+  // spotLight.position.z = Math.cos(angle) * radius;
 
-  spotLightBlue.position.x = Math.cos( time * 0.3 ) * 100;
-  spotLightBlue.position.y = Math.sin( time * 0.5 ) * 100;
-  spotLightBlue.position.z = Math.sin( time * 0.7 ) * 100;
+  //Rotate Discoball - Y is up
+  scene.traverse((child) => {
+    if (child.isMesh) {
+      child.rotation.y += 0.005; // Adjust the speed as needed
+    }
+  });
 
-  spotLightGreen.position.x = Math.sin( time * 0.7 ) * 100;
-  spotLightGreen.position.y = Math.cos( time * 0.3 ) * 100;
-  spotLightGreen.position.z = Math.sin( time * 0.5 ) * 100;
-
-  spotLightOrange.position.x = Math.sin( time * 0.3 ) * 100;
-  spotLightOrange.position.y = Math.cos( time * 0.7 ) * 100;
-  spotLightOrange.position.z = Math.sin( time * 0.5 ) * 100;
-
+  if (lightHelper) lightHelper.update();
   renderer.render(scene, camera);
 }
 
